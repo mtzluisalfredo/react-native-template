ca93d2118efce7b4612c736cb2e277eabd194595
Welington da Silva <thebylito@gmail.com>
2019-07-10 21:38:09 -0400

    initial commit + react-native-navigation(android)

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  "rules": {
+    "react/jsx-filename-extension": [
+      1,
+      {
+        "extensions": [
+          ".js",
+          ".jsx"
+        ]
+      }
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        "trailingComma": "es5",
+        "singleQuote": true,
+        "printWidth": 100
+      }
+    ]
+  },
+  "plugins": [
+    "prettier"
+  ]
+};
---
+++ android/app/src/main/java/com/reactnativetemplate/MainActivity.java
@@ -0,0 +1,6 @@
+package com.reactnativetemplate;
+
+import com.reactnativenavigation.NavigationActivity;
+
+public class MainActivity extends NavigationActivity {
+}
